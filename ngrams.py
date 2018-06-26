df8f436135c5e0dab1488de8a768cb850b6852cb
Jan Kretschmann <jan.kretschmann@rwth-aachen.de>
2018-06-26 14:35:02 +0200

    get ngrams and frequency, added __str__ and __repr__ to class NGram

--- ngrams.py
+++ ngrams.py
@@ -28,19 +28,31 @@ class NGrams:
             t_line = []
             too_short = False
             for line in file:
+                line = line.strip()
                 t_line += self.handle_interpunction(line).split(' ')
                 if too_short:
                     continue
                 self.extract_ngrams_from_line(t_line)
                 t_line = []
+        self.filter()
+
+    def filter(self):
+        keys_to_remove = []
+        for key, value in self.ngrams.items():
+            for key_, value_ in self.ngrams.items():
+                if key.words == key_.words and key.successor != key_.successor:
+                    keys_to_remove.append(key_ if value > value_ else key)
+        for key in keys_to_remove:
+            self.ngrams.pop(key)
 
 
     def extract_ngrams_from_line(self, line):
-        for i in range(0, len(line), self.n):
+        for i in range(len(line)):
             ngram = []
-            for j in range(self.n):
+            for j in range(i, min(self.n+i, len(line))):
                 ngram.append(line[j])
-            self.ngrams.update({ngram:self.ngrams.get(ngram)+1})
+            ngram = NGram(ngram)
+            self.ngrams.update({ngram:self.ngrams.get(ngram, 0)+1})
         
 
     def handle_interpunction(self, s):
@@ -49,4 +61,15 @@ class NGrams:
         s = re.sub('\s{2,}', ' ', s)
         return s
         
+class NGram:
+
+    def __init__(self, words):
+        self.successor = words[-1]
+        self.words = words[:-1]
+
+    def __str__(self):
+        return str(self.words + [self.successor])
+    
+    __repr__ = __str__    
+
 
