daf200432aa8ba2c98e77ba1afed80038d548d6c
Jan Kretschmann <jan.kretschmann@rwth-aachen.de>
2018-06-26 10:27:33 +0200

    io of class ngrams

--- ngrams.py
+++ ngrams.py
@@ -1,7 +1,45 @@
+import re
+
+"""
+Dumb:
+    - For each word: save all predecessing n-grams and their frequency
+    - dict of string, array of strings --> key=word, array of n-grams
+    - dict of string, array of numbers --> key=word, array of frequencies
+
+Maybe smarter:
+    - For each n-sequence of words, save all successing (single) words and their frequency
+    - dict of array of strings, array of strings --> key=ngram, value=successors
+    - dict of array of strings, array of numbers --> key=ngram, value=frequencies of successors
+TODO
+    - Interpunction = ?
+    - save interpunction like words --> ngrams, frequency
+"""
 
 
-def NGrams:
+class NGrams:
 
     def __init__(self, n):
+        self.n = n
         self.ngrams = {}
+        self.frequencies = {}
+
+    def read_file(self, filename):
+        # Handle lines with less than n words
+        with open(filename) as file:
+            for line in file:
+                self.extract_ngrams_from_line(line)
+
+    def extract_ngrams_from_line(self, line):
+        ngrams = []
+        line = self.handle_interpunction(line).split(' ')
+        frequencies = [0] * (len(line)-20)
+        for i in range(0, len(line), self.n):
+            
+            
+    def handle_interpunction(self, s):
+        # Inserts whitespace before each punctuation mark so it gets dealt with like a word
+        s = re.sub('([.,!?()])', r' \1 ', s)
+        s = re.sub('\s{2,}', ' ', s)
+        return s
         
+
