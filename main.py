a18ddea4858ad1f5410072bd4ce53a4fa7532f08
Jan Kretschmann <jan.kretschmann@rwth-aachen.de>
2018-06-26 14:36:19 +0200

    restructuring

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from ngrams import *
 
+"""
+Dumb:
+    - For each word: save all predecessing n-grams and their frequency
+    - dict of string, array of strings --> key=word, array of n-grams
+    - dict of string, array of numbers --> key=word, array of frequencies
 
-
+Maybe smarter:
+    - For each n-sequence of words, save all successing (single) words and their frequency
+    - dict of array of strings, array of strings --> key=ngram, value=successors
+    - dict of array of strings, array of numbers --> key=ngram, value=frequencies of successors
+TODO
+    - Interpunction = ?
+    - save interpunction like words --> ngrams, frequency
+    - Class NGram, List unhashable
+    - Hash ngram only based on internal list, else dict is useless
+"""
 
 
 if __name__=='__main__':
--- ngrams.py
+++ ngrams.py
@@ -1,20 +1,6 @@
 import re
 
-"""
-Dumb:
-    - For each word: save all predecessing n-grams and their frequency
-    - dict of string, array of strings --> key=word, array of n-grams
-    - dict of string, array of numbers --> key=word, array of frequencies
 
-Maybe smarter:
-    - For each n-sequence of words, save all successing (single) words and their frequency
-    - dict of array of strings, array of strings --> key=ngram, value=successors
-    - dict of array of strings, array of numbers --> key=ngram, value=frequencies of successors
-TODO
-    - Interpunction = ?
-    - save interpunction like words --> ngrams, frequency
-    - Class NGram, List unhashable
-"""
 
 
 class NGrams:
